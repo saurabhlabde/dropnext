7d59f06f4a818526f54c48b0c618106e05574be0
saurabhlabde <54150006+saurabhlabde@users.noreply.github.com>
2021-06-14 19:45:46 +0530

    #1 SETUP

---
+++ client/utils/mediaUrl.ts
@@ -0,0 +1,11 @@
+export const mediaUrl = (e: any) => {
+        const input_id = (<HTMLInputElement>document.getElementById(`${e.target.id}`))
+                .files[0];
+
+        if (!input_id) return;
+
+        const ConvertFile = URL.createObjectURL(input_id);
+        if (ConvertFile) {
+                return ConvertFile;
+        }
+};
